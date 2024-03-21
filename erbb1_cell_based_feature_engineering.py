97df8753a08fb2c5fd635627a4f014aac74c5bb5
Zach-1147 <144257940+Zach-1147@users.noreply.github.com>
2024-03-21 13:56:18 -0400

    Created script for feature engineergin

---
+++ erbb1_cell_based_feature_engineering.py
@@ -0,0 +1,11 @@
+
+## ---------------------------------------------------------------- ##
+## --------- Feature Engineering with Cell Based Format ----------- ##
+## ---------------------------------------------------------------- ##
+
+import numpy as np
+import pandas as pd
+import plotly.graph_objects as go
+
+erbb1_df = pd.read_csv("erbb1_cellbased_neglog10_ic50.csv")
+
